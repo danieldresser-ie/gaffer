22a0aaa0bbd0b178078b15c751805f9650fc0fee
Daniel Dresser <danield@image-engine.com>
2025-11-04 19:39:37 -0800

    WIP

--- src/GafferScene/Cube.cpp
+++ src/GafferScene/Cube.cpp
@@ -44,69 +44,117 @@ using namespace Imath;
 using namespace IECore;
 using namespace IECoreScene;
 
-GAFFER_NODE_DEFINE_TYPE( Cube );
+namespace {
 
-size_t Cube::g_firstPlugIndex = 0;
-
-Cube::Cube( const std::string &name )
-	:	ObjectSource( name, "cube" )
+void addCorner( const V3f &p, std::vector< V3f > &ps, std::vector< std::vector< int > > &perFaceIndices, const std::vector< Imath::V2i > &faceSizes ) //, std::tuple< Imath::V3i, Imath::V3i, Imath::V3i > inds )
 {
-	storeIndexOfNextChild( g_firstPlugIndex );
-	addChild( new V3fPlug( "dimensions", Plug::In, V3f( 1.0f ), V3f( 0.0f ) ) );
-	addChild( new V3iPlug( "divisions", Plug::In, V3i( 1 ), V3i( 1 ) ) );
+	int cornerIndex  = ps.size();
+	ps.push_back( p );
+
+	int xFace = p.x != 0.0f;
+	perFaceIndices[ xFace ][
+		( p.y > 0 ) * ( faceSizes[ xFace ].x - 1 ) +
+		( p.z > 0 ) * ( faceSizes[ xFace ].y - 1 ) * faceSizes[ xFace ].x
+	] = cornerIndex;
+	int yFace = 2 + ( p.y != 0.0f );
+	perFaceIndices[ yFace ][
+		( p.y > 0 ) * ( faceSizes[ yFace ].x - 1 ) +
+		( p.z > 0 ) * ( faceSizes[ yFace ].y - 1 ) * faceSizes[ yFace ].x
+	] = cornerIndex;
+	int zFace = 4 + ( p.z != 0.0f );
+	perFaceIndices[ zFace ][
+		( p.y > 0 ) * ( faceSizes[ zFace ].x - 1 ) +
+		( p.z > 0 ) * ( faceSizes[ zFace ].y - 1 ) * faceSizes[ zFace ].x
+	] = cornerIndex;
 }
 
-Cube::~Cube()
+void addXEdge( const V3f &p, std::vector< V3f > &ps, std::vector< std::vector< int > > &perFaceIndices, const V3i &vertsPer )
 {
-}
-
-Gaffer::V3fPlug *Cube::dimensionsPlug()
-{
-	return getChild<V3fPlug>( g_firstPlugIndex );
-}
-
-const Gaffer::V3fPlug *Cube::dimensionsPlug() const
-{
-	return getChild<V3fPlug>( g_firstPlugIndex );
-}
-
-Gaffer::V3iPlug *Cube::divisionsPlug()
-{
-	return getChild<V3iPlug>( g_firstPlugIndex + 1 );
-}
-
-const Gaffer::V3iPlug *Cube::divisionsPlug() const
-{
-	return getChild<V3iPlug>( g_firstPlugIndex + 1 );
-}
-
-void Cube::affects( const Plug *input, AffectedPlugsContainer &outputs ) const
-{
-	ObjectSource::affects( input, outputs );
-
-	if(
-		input->parent<V3fPlug>() == dimensionsPlug() ||
-		input->parent<V3fPlug>() == divisionsPlug()
-	)
+	for( int i = 1; i < vertsPer.x - 1; i++ )
 	{
-		outputs.push_back( sourcePlug() );
+		int edgeIndex = ps.size();
+		ps.push_back( V3f( i / float( vertsPer.x - 1 ), p.y, p.z ) );
+
+		perFaceIndices[ 2 + ( p.y != 0.0f ) ][ i + ( p.z > 0 ) * ( vertsPer.z - 1 ) * vertsPer.x ] = edgeIndex;
+		perFaceIndices[ 4 + ( p.z != 0.0f ) ][ i + ( p.y > 0 ) * ( vertsPer.y - 1 ) * vertsPer.x ] = edgeIndex;
 	}
 }
 
-void Cube::hashSource( const Gaffer::Context *context, IECore::MurmurHash &h ) const
+void addYEdge( const V3f &p, std::vector< V3f > &ps, std::vector< std::vector< int > > &perFaceIndices, const V3i &vertsPer )
 {
-	dimensionsPlug()->hash( h );
-	divisionsPlug()->hash( h );
+	for( int i = 1; i < vertsPer.y - 1; i++ )
+	{
+		int edgeIndex = ps.size();
+		ps.push_back( V3f( p.x, i / float( vertsPer.y - 1 ), p.z ) );
+
+		perFaceIndices[ ( p.x != 0.0f ) ][ i + ( p.z > 0 ) * ( vertsPer.z - 1 ) * vertsPer.y ] = edgeIndex;
+		perFaceIndices[ 4 + ( p.z != 0.0f ) ][ i * vertsPer.x + ( p.x > 0 ) * ( vertsPer.x - 1 ) ] = edgeIndex;
+	}
 }
 
-IECore::ConstObjectPtr Cube::computeSource( const Context *context ) const
+void addZEdge( const V3f &p, std::vector< V3f > &ps, std::vector< std::vector< int > > &perFaceIndices, const V3i &vertsPer )
 {
-	V3f dimensions = dimensionsPlug()->getValue();
-	V3i divisions = divisionsPlug()->getValue();
+	for( int i = 1; i < vertsPer.z - 1; i++ )
+	{
+		int edgeIndex = ps.size();
+		ps.push_back( V3f( p.x, p.y, i / float( vertsPer.z - 1 ) ) );
+
+		perFaceIndices[ ( p.x != 0.0f ) ][ i * vertsPer.y + ( p.y > 0 ) * ( vertsPer.y - 1 ) ] = edgeIndex;
+		perFaceIndices[ 2 + ( p.y != 0.0f ) ][ i * vertsPer.x + ( p.x > 0 ) * ( vertsPer.x - 1 ) ] = edgeIndex;
+	}
+}
+
+MeshPrimitivePtr createDividedBox( const Box3f &b, const Imath::V3f &divisions )
+{
+	V3i vertsPer = divisions + V3i(1);
+
+	std::vector< Imath::V2i > faceSizes;
+	faceSizes.push_back( Imath::V2i( vertsPer[1], vertsPer[2] ) );
+	faceSizes.push_back( Imath::V2i( vertsPer[1], vertsPer[2] ) );
+	faceSizes.push_back( Imath::V2i( vertsPer[0], vertsPer[2] ) );
+	faceSizes.push_back( Imath::V2i( vertsPer[0], vertsPer[2] ) );
+	faceSizes.push_back( Imath::V2i( vertsPer[0], vertsPer[1] ) );
+	faceSizes.push_back( Imath::V2i( vertsPer[0], vertsPer[1] ) );
+
+	std::vector< std::vector< int > > perFaceIndices;
+
+	perFaceIndices.resize( 6 );
+	//size_t numFaces = 0;
+	for( int i = 0; i < 6; i++ )
+	{
+		perFaceIndices[i].resize( faceSizes[i].x * faceSizes[i].y, -1 ); // TODO - no init
+		//numFaces += faceSizes[i].x * faceSizes[i].y; NOT
+	}
+
+	V3fVectorDataPtr pData = new V3fVectorData;
+	std::vector< V3f > &ps = pData->writable();
+
+	addCorner( V3f( 0, 0, 0 ), ps, perFaceIndices, faceSizes );
+	addCorner( V3f( 1, 0, 0 ), ps, perFaceIndices, faceSizes );
+	addCorner( V3f( 1, 1, 0 ), ps, perFaceIndices, faceSizes );
+	addCorner( V3f( 0, 1, 0 ), ps, perFaceIndices, faceSizes );
+	addCorner( V3f( 1, 0, 1 ), ps, perFaceIndices, faceSizes );
+	addCorner( V3f( 1, 1, 1 ), ps, perFaceIndices, faceSizes );
+	addCorner( V3f( 0, 0, 1 ), ps, perFaceIndices, faceSizes );
+	addCorner( V3f( 0, 1, 1 ), ps, perFaceIndices, faceSizes );
+
+	addXEdge( V3f( 0, 0, 0 ), ps, perFaceIndices, vertsPer );
+	addXEdge( V3f( 0, 0, 1 ), ps, perFaceIndices, vertsPer );
+	addXEdge( V3f( 0, 1, 0 ), ps, perFaceIndices, vertsPer );
+	addXEdge( V3f( 0, 1, 1 ), ps, perFaceIndices, vertsPer );
+
+	addYEdge( V3f( 0, 0, 0 ), ps, perFaceIndices, vertsPer );
+	addYEdge( V3f( 0, 0, 1 ), ps, perFaceIndices, vertsPer );
+	addYEdge( V3f( 1, 0, 0 ), ps, perFaceIndices, vertsPer );
+	addYEdge( V3f( 1, 0, 1 ), ps, perFaceIndices, vertsPer );
+
+	addZEdge( V3f( 0, 0, 0 ), ps, perFaceIndices, vertsPer );
+	addZEdge( V3f( 0, 1, 0 ), ps, perFaceIndices, vertsPer );
+	addZEdge( V3f( 1, 0, 0 ), ps, perFaceIndices, vertsPer );
+	addZEdge( V3f( 1, 1, 0 ), ps, perFaceIndices, vertsPer );
 
    std::string interpolation = "linear";
-	vector< V3f > p;
-	std::vector<int> verticesPerFace {
+	/*std::vector<int> verticesPerFace {
 		4, 4, 4, 4, 4, 4
 	};
 	std::vector<int> vertexIds {
@@ -126,10 +174,32 @@ IECore::ConstObjectPtr Cube::computeSource( const Context *context ) const
 	p.push_back( V3f( b.max.x, b.max.y, b.max.z ) );    // 5
 	p.push_back( V3f( b.min.x, b.min.y, b.max.z ) );    // 6
 	p.push_back( V3f( b.min.x, b.max.y, b.max.z ) );    // 7
+	*/
 
-	MeshPrimitivePtr result = new MeshPrimitive( new IntVectorData(verticesPerFace), new IntVectorData(vertexIds), interpolation, new V3fVectorData(p) );
+	for( V3f &p : ps )
+	{
+		p = b.min + b.size() * p ;
+	}
 
-	V2fVectorDataPtr uvData = new V2fVectorData;
+	MeshPrimitivePtr result = new MeshPrimitive( new IntVectorData(), new IntVectorData(), interpolation, pData );
+
+
+	std::cerr << "INTERMEDIATE INDICES";
+
+	for( size_t i = 0; i < perFaceIndices.size(); i++ )
+	{
+		std::cerr << "\n\n";
+		for( int y = 0; y < faceSizes[i].y; y++ )
+		{
+			for( int x = 0; x < faceSizes[i].x; x++ )
+			{
+				std::cerr << perFaceIndices[i][ y * faceSizes[i].x + x ] << " ";
+			}
+			std::cerr << "\n";
+		}
+	}
+
+	/*V2fVectorDataPtr uvData = new V2fVectorData;
 	uvData->setInterpretation( GeometricData::UV );
 	std::vector<Imath::V2f> &uvs = uvData->writable();
 
@@ -175,7 +245,72 @@ IECore::ConstObjectPtr Cube::computeSource( const Context *context ) const
 		3,3,3,3,
 	};
 
-	result->variables["N"] = PrimitiveVariable( PrimitiveVariable::FaceVarying, new V3fVectorData( normals, GeometricData::Normal ), new IntVectorData ( nIndices ) );
+	result->variables["N"] = PrimitiveVariable( PrimitiveVariable::FaceVarying, new V3fVectorData( normals, GeometricData::Normal ), new IntVectorData ( nIndices ) );*/
 
 	return result;
+}
+
+} // namespace
+
+GAFFER_NODE_DEFINE_TYPE( Cube );
+
+size_t Cube::g_firstPlugIndex = 0;
+
+Cube::Cube( const std::string &name )
+	:	ObjectSource( name, "cube" )
+{
+	storeIndexOfNextChild( g_firstPlugIndex );
+	addChild( new V3fPlug( "dimensions", Plug::In, V3f( 1.0f ), V3f( 0.0f ) ) );
+	addChild( new V3iPlug( "divisions", Plug::In, V3i( 1 ), V3i( 1 ) ) );
+}
+
+Cube::~Cube()
+{
+}
+
+Gaffer::V3fPlug *Cube::dimensionsPlug()
+{
+	return getChild<V3fPlug>( g_firstPlugIndex );
+}
+
+const Gaffer::V3fPlug *Cube::dimensionsPlug() const
+{
+	return getChild<V3fPlug>( g_firstPlugIndex );
+}
+
+Gaffer::V3iPlug *Cube::divisionsPlug()
+{
+	return getChild<V3iPlug>( g_firstPlugIndex + 1 );
+}
+
+const Gaffer::V3iPlug *Cube::divisionsPlug() const
+{
+	return getChild<V3iPlug>( g_firstPlugIndex + 1 );
+}
+
+void Cube::affects( const Plug *input, AffectedPlugsContainer &outputs ) const
+{
+	ObjectSource::affects( input, outputs );
+
+	if(
+		input->parent<V3fPlug>() == dimensionsPlug() ||
+		input->parent<V3iPlug>() == divisionsPlug()
+	)
+	{
+		outputs.push_back( sourcePlug() );
+	}
+}
+
+void Cube::hashSource( const Gaffer::Context *context, IECore::MurmurHash &h ) const
+{
+	dimensionsPlug()->hash( h );
+	divisionsPlug()->hash( h );
+}
+
+IECore::ConstObjectPtr Cube::computeSource( const Context *context ) const
+{
+	V3f dimensions = dimensionsPlug()->getValue();
+	V3i divisions = divisionsPlug()->getValue();
+
+	return createDividedBox( Box3f( -dimensions / 2.0f, dimensions / 2.0f ), divisions );
 }
